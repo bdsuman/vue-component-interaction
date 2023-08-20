7e931dcd4a97665483c7619508799072e7c15590
Suman Sen <mesuman@yahoo.com>
2023-08-20 22:38:40 +0600

    complated

---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive,ref } from 'vue'
+import ChildComponent from './ChildComponent.vue';
+
+const persons = reactive({
+  'name'  : 'Suman',
+  'age'   : 26,
+  'message'   : 'hey im a web developer with 4 years of experience'
+});
+</script>
+
+<template>
+  <div class="mt-10">
+    <h1 class="text-black-900 text-xl my-5">Parent Component:</h1>
+    <h2 class="text-black text-l ">Name : {{ persons.name }}</h2>
+    <h2 class="text-black text-l ">Age : {{ persons.age }}</h2>
+    <h2 class="text-black text-l ">Message : {{ persons.message }}</h2>
+  </div>
+
+  <ChildComponent v-model:message="persons.message" ></ChildComponent>
+</template>
+
+<style scoped>
+
+</style>
